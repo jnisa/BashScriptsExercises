63cae6171fe1317a23f8945dca06c6980de5d4b9
João Nisa <joao.je.nisa@gmail.com>
2021-06-25 09:41:38 +0100

    add a rotation layer on the latest exercise

--- PickTheLatestFolder/move_hive_logs.sh
+++ PickTheLatestFolder/move_hive_logs.sh
@@ -3,6 +3,7 @@
 initialPath=/Users/jnisa/Desktop/Projects/BashScriptsExercises/PickTheLatestFolder/PlayGround/
 startingPoint=startingPoint/
 endPoint=endPoint/
+retentionPeriod=1
 
 # go to the log directory
 cd "$initialPath$startingPoint"
@@ -14,7 +15,19 @@ latestLogsFolder=$(ls -Art | tail -n 1)
 if [[ -e "$initialPath$endPoint$latestLogsFolder" ]]
 then
  echo "There's no new files to be copied!"
- # place your copy command here
 else
  echo "There's new files to be copied!"
+ cp -a "$initialPath$startingPoint$latestLogsFolder" "$initialPath$endPoint"
+fi
+
+# set the rotation of those logs 
+count=$(find "$initialPath$endPoint" -maxdepth 1 -mindepth 1 | wc -l)
+echo $count
+
+if [ "$count" -gt "$retentionPeriod" ]
+then
+ find "$initialPath$endPoint" -maxdepth 1 -mindepth 1 -name "*" -mtime "$retentionPeriod" -exec rm -rf {} \;
+ echo "hdfs logs have been rotated!" 
+else
+ echo "no hdfs logs to be rotated!"
 fi 
